f0f8b9a8b1ab5d639105cda0fefc8fc91d4242f7
lee <ljp_20131201@163.com>
2016-12-29 16:30:57 +0800

    firt commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Miguel'}
+    return render_template("index.html",
+        title = 'Home',
+        user = user)
